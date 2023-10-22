a16a95eb872ec89fcf7540a51916df8c90930ab2
daryaeremeeva <daryaeremeeva2013@mail.ru>
2023-10-22 20:47:21 +0300

    add tools.h code

--- constants.h
+++ constants.h
@@ -1,5 +1,7 @@
 #ifndef CONSTANTS
 #define CONSTANTS
 
+#define MAP_HEIGHT 21
+
 #endif // CONSTANTS
 
--- tools.h
+++ tools.h
@@ -2,10 +2,18 @@
 #define TOOLS_H
 
 
-class tools
-{
-public:
-    tools();
-};
+
+#include "game_types.h"
+#include "pacman.h"
+#include "score.h"
+
+game_map make_game_map(const std::array<std::string, MAP_HEIGHT> &map_from_strings, std::array<position, 4> &ghost_positions, pacman &pacman);
+
+void free_map(game_map p);
+
+bool map_collision(bool use_door, position point, game_map& map);
+
+bool collect_pellets(position point, game_map &map, score &score, int level);
 
 #endif // TOOLS_H
+
