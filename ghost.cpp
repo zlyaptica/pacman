fbf088eedd64468282a99d2648c866861f714085
AlexBlack1338 <like4311@gmail.com>
2023-10-23 17:11:12 +0300

    feature_ghost PART 2. add some ghost methods

--- ghost.cpp
+++ ghost.cpp
@@ -6,12 +6,34 @@ ghost::ghost(byte id) : id(id)
 
 bool ghost::pacman_collision(const position &pacman_position)
 {
+    if (cur_position.x > pacman_position.x - CELL_SIZE && cur_position.x < CELL_SIZE + pacman_position.x)
+        if (cur_position.y > pacman_position.y - CELL_SIZE && cur_position.y < CELL_SIZE + pacman_position.y)
+            return 1;
 
+    return 0;
 }
 
 float ghost::get_target_distance(byte direction)
 {
+    short x = cur_position.x;
+    short y = cur_position.y;
 
+    switch (direction)
+    {
+    case 0:
+        x += GHOST_SPEED;
+        break;
+    case 1:
+        y -= GHOST_SPEED;
+        break;
+    case 2:
+        x -= GHOST_SPEED;
+        break;
+    case 3:
+        y += GHOST_SPEED;
+    }
+
+    return (float)(sqrt(pow(x - target.x, 2) + pow(y - target.y, 2)));
 }
 
 void ghost::draw(bool flash, sf::RenderWindow &window)
@@ -26,12 +48,12 @@ void ghost::reset(const position &home, const position &home_exit)
 
 void ghost::switch_mode()
 {
-
+    movement_mode = 1 - movement_mode;
 }
 
 byte flip_direction(byte direction)
 {
-
+    return (2 + direction) % 4;
 }
 
 void ghost::update(byte level, game_map &map, ghost &ghost_0, pacman &pacman, std::vector<bomb> &bombs)
