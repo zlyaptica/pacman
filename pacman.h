8ac655e465df9bfd608acf1095dcbb4bd174c445
Nokilay318 <botko2002@mail.ru>
2023-10-23 17:21:33 +0300

    create pacman.h

--- pacman.h
+++ pacman.h
@@ -2,10 +2,30 @@
 #define PACMAN_H
 
 
+#include "game_types.h"
+#include "score.h"
+
 class pacman
 {
 public:
+    bool is_animation_over;
+
+    bool is_dead;
+
+    byte direction;
+
+    unsigned short animation_timer;
+    unsigned short energizer_timer;
+
+    position cur_position;
+
+    score score;
+
     pacman();
+
+    void draw(bool victory, sf::RenderWindow &window);
+    void reset();
+    void update(byte level, game_map &map);
 };
 
 #endif // PACMAN_H
