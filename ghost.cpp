7c78b38b6670bb94a5390678cfaab0784cd543b1
AlexBlack1338 <like4311@gmail.com>
2023-10-23 10:49:00 +0300

    feature_ghost PART 1. create ghost.h, add methods in ghost.cpp

--- ghost.cpp
+++ ghost.cpp
@@ -1,7 +1,45 @@
 #include "ghost.h"
 
-ghost::ghost()
+ghost::ghost(byte id) : id(id)
+{
+}
+
+bool ghost::pacman_collision(const position &pacman_position)
 {
 
 }
 
+float ghost::get_target_distance(byte direction)
+{
+
+}
+
+void ghost::draw(bool flash, sf::RenderWindow &window)
+{
+
+}
+
+void ghost::reset(const position &home, const position &home_exit)
+{
+
+}
+
+void ghost::switch_mode()
+{
+
+}
+
+byte flip_direction(byte direction)
+{
+
+}
+
+void ghost::update(byte level, game_map &map, ghost &ghost_0, pacman &pacman, std::vector<bomb> &bombs)
+{
+
+}
+
+void ghost::update_target(byte pacman_direction, const position &ghost_0_position, const position &pacman_position)
+{
+
+}
